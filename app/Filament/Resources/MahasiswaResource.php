0c50528726b92cbff3ef17cad7cbcbd4ca0d3de3
robithrivaldy <robith.rivaldy2013@gmail.com>
2023-07-02 14:40:41 +0700

    first commit

---
+++ app/Filament/Resources/MahasiswaResource.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MahasiswaResource\Pages;
+use App\Filament\Resources\MahasiswaResource\RelationManagers;
+use App\Models\Mahasiswa;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MahasiswaResource extends Resource
+{
+    protected static ?string $model = Mahasiswa::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name'),
+                TextInput::make('address')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('address'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMahasiswas::route('/'),
+            'create' => Pages\CreateMahasiswa::route('/create'),
+            'edit' => Pages\EditMahasiswa::route('/{record}/edit'),
+        ];
+    }
+}
